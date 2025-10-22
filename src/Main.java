6f5dc372ed693dcb1f03fb52a3cc728fa5ff67b6
MichaelNikoloudakis <michaeln273@nycstudents.net>
2025-10-22 12:57:07 -0400

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        WordMatch match = new WordMatch("ssomething");
-        System.out.println(match.scoreGuess("s"));
+        WordMatch game = new WordMatch("mississippi");
+        System.out.println(game.scoreGuess("i"));
+        System.out.println(game.scoreGuess("iss"));
+        System.out.println(game.scoreGuess("issipp"));
+        System.out.println(game.scoreGuess("mississippi"));
+
+        game = new WordMatch("aaaabb");
+        System.out.println(game.scoreGuess("a"));
+        System.out.println(game.scoreGuess("aa"));
+        System.out.println(game.scoreGuess("aaa"));
+        System.out.println(game.scoreGuess("aabb"));
+        System.out.println(game.scoreGuess("c"));
+
+        game = new WordMatch("concatenation");
+        System.out.println(game.scoreGuess("ten"));
+        System.out.println(game.scoreGuess("nation"));
+        System.out.println(game.findBetterGuess("ten" , "nation"));
+        System.out.println(game.scoreGuess("con"));
+        System.out.println(game.scoreGuess("cat"));
+        System.out.println(game.findBetterGuess("con" , "cat"));
     }
 }
--- src/WordMatch.java
+++ src/WordMatch.java
@@ -8,21 +8,20 @@ public class WordMatch {
 
     public int scoreGuess(String guess)
     {
-        int subtrs = 0;
-
-        for (int x = 0; x < this.secret.length(); x++) {
-            for (int y = x + 1; y < this.secret.length() - 1; y++) {
-                if (this.secret.substring(x, y).equals(guess)) {
-                    subtrs++;
-                }
+        int score = 0;
+        for (int i = 0; i <= secret.length() - guess.length(); i++) {
+            if (this.secret.substring(i, i + guess.length()).equals(guess)) {
+                score++;
             }
         }
 
-        return subtrs;
+        return (score * guess.length() * guess.length());
     }
 
     public String findBetterGuess(String guess1, String guess2)
     {
-        return "";
+        int guess_1 = this.scoreGuess(guess1);
+        int guess_2 = this.scoreGuess(guess2);
+        return guess_1 > guess_2 ? guess2 : guess1;
     }
 }
